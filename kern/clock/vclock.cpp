e03d82879d4860261c71b731d2638f7861ed0a14
ljt12138 <ljt12138@qq.com>
2019-04-26 23:00:04 +0800

    feat(vclock.cpp): two clocks!

--- kern/clock/vclock.cpp
+++ kern/clock/vclock.cpp
@@ -14,19 +14,38 @@ using std::unique_lock;
 using std::cout;
 using std::endl;
 
+mutex ticker;
+
 static void thread_main()
 {
 	intr_info info;
 	info.intr_id = clock_intr;
 	while (1) {
 		sleep(1);
+		ticker.lock();
+		cout << "ticker 1" << endl;
 		intr_alert(&info);
+		ticker.unlock();
+	}
+}
+
+static void thread_main_two()
+{
+	intr_info info;
+	info.intr_id = clock_intr;
+	while (1) {
+		sleep(2);
+		ticker.lock();
+		cout << "ticker 2" << endl;
+		intr_alert(&info);
+		ticker.unlock();
 	}
 }
 
 void clock_init()
 {
 	cout << "init clock." << endl;
-	thread th(thread_main);
+	thread th(thread_main), th2(thread_main_two);
 	th.join();
+	th2.join();
 }
