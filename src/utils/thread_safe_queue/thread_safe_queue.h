886ba62e3266eec07c4e4cf92e57b0cd6e28a8f7
tqyaaaaang <tqyangoi1@gmail.com>
2019-04-29 12:39:52 +0800

    feat(utils): modify thread-safe queue to allow double-end operations
    Modify thread-safe queue to allow double-end operations

--- src/utils/thread_safe_queue/thread_safe_queue.h
+++ src/utils/thread_safe_queue/thread_safe_queue.h
@@ -13,15 +13,64 @@ template < typename T >
 class thread_safe_queue
 {
 public:
-	void push ( T t );
-	T pop ();
+	void push_back ( T t );
+	void push_front ( T t );
+	T pop_back ();
+	T pop_front ();
 
 	bool empty ();
 
-	void notify ();
-
 private:
-	std::queue < T > q;
+	std::deque < T > q;
 	mutable std::mutex mut;
 	std::condition_variable cv;
 };
+
+
+
+template<typename T>
+void thread_safe_queue<T>::push_back ( T t )
+{
+	std::unique_lock < std::mutex > lock ( mut );
+	q.push_back ( t );
+	cv.notify_one ();
+}
+
+template<typename T>
+void thread_safe_queue<T>::push_front ( T t )
+{
+	std::unique_lock < std::mutex > lock ( mut );
+	q.push_front ( t );
+	cv.notify_one ();
+}
+
+template<typename T>
+T thread_safe_queue<T>::pop_back ()
+{
+	std::unique_lock < std::mutex > lock ( mut );
+	while ( q.empty () ) {
+		cv.wait ( lock );
+	}
+	T return_value = q.back ();
+	q.pop_back ();
+	return return_value;
+}
+
+template<typename T>
+T thread_safe_queue<T>::pop_front ()
+{
+	std::unique_lock < std::mutex > lock ( mut );
+	while ( q.empty () ) {
+		cv.wait ( lock );
+	}
+	T return_value = q.front ();
+	q.pop_front ();
+	return return_value;
+}
+
+template<typename T>
+bool thread_safe_queue<T>::empty ()
+{
+	std::unique_lock < std::mutex > lock ( mut );
+	return q.empty ();
+}
