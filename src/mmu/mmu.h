010b3939abcce6a2f2f965a976fa1c1c8fcf3709
tqyaaaaang <tqyangoi1@gmail.com>
2019-04-30 23:53:56 +0800

    feat(mmu): add mmu.h

---
+++ src/mmu/mmu.h
@@ -0,0 +1,22 @@
+/*
+* Memory Manage Unit
+* Protection: when process access invalid address, trigger a #GP
+* Paging: when process access a page not loaded in memory, trigger a #PF
+*/
+
+/**
+* class CPU_mmu
+* Each instance of this class is a mmu of a core
+*/
+class CPU_mmu
+{
+public:
+	struct mmu_result
+	{
+
+	}
+	void read ( size_t la );
+	void write ( size_t la, char data );
+private:
+
+};
