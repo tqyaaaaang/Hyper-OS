0e47298eb38351816f6792a6edab4d7da4bb5d35
ljt12138 <390402616@qq.com>
2019-05-19 08:27:16 -0700

    fix(panic): fix panic
    TODO
    (1): swap in/swap out
    (2): kill process
    (3): create proc & exec syscall

--- src/utils/panic.cpp
+++ src/utils/panic.cpp
@@ -1,10 +1,12 @@
 #include "panic.h"
+#include <cstdlib>
 #include <string>
 using std::string;
 
 void panic(const string &str)
 {
 	logging::debug << "System Panic. info = " << str << logging::log_endl;
+	exit(1);
 }
 
 void panic()
