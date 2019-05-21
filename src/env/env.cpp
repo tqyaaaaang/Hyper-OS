de4bdb8542684d6e4f8abf9a740077723c1613a4
tqyaaaaang <tqyangoi1@gmail.com>
2019-05-21 14:18:17 +0800

    chore(log): output logging messages to hos.log by default
    Output logging messages to hos.log by default

--- src/env/env.cpp
+++ src/env/env.cpp
@@ -16,6 +16,6 @@ const size_t MEMORY_SIZE = ( size_t ) 1 << 30;   // The size of the physical mem
 const int MOTHERBOARD_NUM = 1;   // The number of motherboards
 
 // Logging
-const char *LOG_FILE_NAME = NULL;   // File name to write log, NULL if write to stdout
+const char *LOG_FILE_NAME = "hos.log";   // File name to write log, NULL if write to stdout
 
-const int LOG_LEVEL = LOGGING_LEVEL::INFO;   // Logging level to be used
+const int LOG_LEVEL = LOGGING_LEVEL::DEBUG;   // Logging level to be used
