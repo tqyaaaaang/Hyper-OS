3b879d8f51c9d70da3de446426c5d210ce8ce404
tqyaaaaang <tqyangoi1@gmail.com>
2019-06-14 15:59:30 +0800

    feat(dev.output): handle special keys while writing to screen service
    Handle special keys while writing to screen service

--- dev/devices/output/output_screen.cpp
+++ dev/devices/output/output_screen.cpp
@@ -7,6 +7,7 @@
 #include "output_screen.h"
 #include "../../../src/logging/logging.h"
 #include "../../device_list.h"
+#include "../../../src/io/special_keys.h"
 
 dev_output_screen::dev_output_screen ( int __motherboard_id )
 	: dev_output ( __motherboard_id )
@@ -25,19 +26,29 @@ void dev_output_screen::destroy ()
 
 void dev_output_screen::write ( int ch )
 {
-	if ( ch >= 32 ) {
-		logging::debug << "Device output received character '" << ch << "' ( ASCII " << static_cast < int > ( ch ) << " ) " << logging::log_endl;
-	} else {
-		logging::debug << "Device output received unvisible character ( ASCII " << static_cast < int > ( ch ) << " ) " << logging::log_endl;
-	}
+	if ( ch >= 0 ) {
+		if ( ch >= 32 ) {
+			logging::debug << "Device output received character '" << ch << "' ( ASCII " << static_cast < int > ( ch ) << " ) " << logging::log_endl;
+		} else {
+			logging::debug << "Device output received unvisible character ( ASCII " << static_cast < int > ( ch ) << " ) " << logging::log_endl;
+		}
 
-	if ( ch == '\b' ) {   // handle backspace
-		device_desc::device_screen->write ( "c", "backspace" );
-	} else if ( ch == '\n' ) {
-		device_desc::device_screen->write ( "c", "newline" );
+		if ( ch == '\b' ) {   // handle backspace
+			device_desc::device_screen->write ( "c", "backspace" );
+		} else if ( ch == '\n' ) {
+			device_desc::device_screen->write ( "c", "newline" );
+		} else {
+			std::string data;
+			data = ch;
+			device_desc::device_screen->write ( "w", data );
+		}
 	} else {
-		std::string data;
-		data = ch;
-		device_desc::device_screen->write ( "w", data );
+		special_keys key = static_cast < special_keys > ( -ch );
+		logging::debug << "Device output received special key " << get_key_str ( key ) << logging::log_endl;
+
+		switch ( key ) {
+		default:
+			logging::warning << "Special key " << get_key_str ( key ) << " not handled" << logging::log_endl;
+		}
 	}
 }
